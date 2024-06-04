2719c42ee70aba0dd563100b0fd8645aadb6d510
Gustavo Amaral <gugamaral@gmail.com>
2024-06-04 19:19:19 -0300

    Versão 1.2

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import os
 
-chaves = ('Nome', 'CPF', 'Telefone', 'E-mail', 'Profissão', 'Empresa')
-cadastros = []
+campos = ('Nome', 'CPF', 'Telefone', 'E-mail', 'Profissão', 'Empresa')
+
+usuarios = []
 
 
 while True:
+    print('1 - Cadastrar usuário.')
+    print('2 - Listar usuários.')
+    print('3 - Alterar usuário.')
+    print('4 - Cadastrar usuário.')
+    print('5 - Cadastrar usuário.')
+    print('6 - Cadastrar usuário.')
+    
+    
     novo_cadastro = input('Inserir novo cadastro de pessoa na lista: ')
 
     if novo_cadastro != '':
